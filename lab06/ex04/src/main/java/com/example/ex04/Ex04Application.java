527e9dbf4fb6725890ffb7d6271b0553119a3772
TranVanThao <vanthao000009@gmail.com>
2023-03-22 18:06:58 +0700

    done lab06

--- lab06/ex04/src/main/java/com/example/ex04/Ex04Application.java
+++ lab06/ex04/src/main/java/com/example/ex04/Ex04Application.java
@@ -1,7 +1,5 @@
 package com.example.ex04;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
