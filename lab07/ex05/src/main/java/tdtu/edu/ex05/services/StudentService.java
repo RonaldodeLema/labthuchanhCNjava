11bd10522f7af53a45fa6053965d335a4c86d31c
TranVanThao <vanthao000009@gmail.com>
2023-03-25 13:13:55 +0700

    bai 5

---
+++ lab07/ex05/src/main/java/tdtu/edu/ex05/services/StudentService.java
@@ -0,0 +1,16 @@
+package tdtu.edu.ex05.services;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import tdtu.edu.ex05.models.Student;
+import tdtu.edu.ex05.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface StudentService extends StudentRepository {
+    @Query(value = "select s from Student s where s.age =:x")
+    public Iterable<Student> cau5a (Integer x);
+    @Query(value = "select s from Student s where s.ieltsScore=:x")
+    public Iterable<Student> cau5b(Double x);
+    @Query(value = "select s from Student s where s.name like %:xxx%")
+    public Iterable<Student> cau5c(String xxx);
+}
---
+++ lab07/ex05/src/main/java/tdtu/edu/ex05/services/StudentServiceImp.java
@@ -0,0 +1,25 @@
+package tdtu.edu.ex05.services;
+
+import tdtu.edu.ex05.models.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class StudentServiceImp {
+    @Autowired
+    private StudentService studentService;
+    public void save(Student student) {
+        studentService.save(student);
+    }
+    public Iterable<Student> cau5a(int x){
+        return studentService.cau5a(x);
+    }
+    public Iterable<Student> cau5b(Double x){
+        return studentService.cau5b(x);
+    }
+    public Iterable<Student> cau5c(String xxx){
+        return studentService.cau5c(xxx);
+    }
+}
